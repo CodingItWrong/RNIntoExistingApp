edf26113c8b1758eb09a9dc3e9c8dac5a9df0f4e
Josh Justice <jjustice@bignerdranch.com>
2018-02-15 15:01:26 -0500

    Create project

---
+++ RNIntoExistingApp/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  RNIntoExistingApp
+//
+//  Created by Josh Justice on 2/15/18.
+//  Copyright © 2018 Josh Justice. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
